9322143f1ba2a5607292bd7f0275472bfa4b2572
hrosan <henriquerosan@gmail.com>
2023-06-19 14:43:27 -0300

    Added one more example about isinstance()

--- Section4/20thClass_isinstance.py
+++ Section4/20thClass_isinstance.py
@@ -39,3 +39,22 @@ my_tuple = (1, 2, 3)
 print(isinstance(my_list, (list, dict)))   # Output: True
 print(isinstance(my_dict, (list, dict)))   # Output: True
 print(isinstance(my_tuple, (list, dict)))  # Output: False
+
+# Common example
+list_ = [
+    'a',1,1.1,True,[0,1,2],(3,5),
+    {7,5},{'nome':'Henrique'}
+]
+
+for item in list_:
+    # From here now the use of isinstance(arg,class)
+    if isinstance(item,bool): # Print every bool data
+        print(item)
+    
+    if isinstance(item,list): # Print if a list
+        item.append(7) # adding a number just to show
+        print(item)
+
+    if isinstance(item,(int,float)): #checking if item is int or float
+        item = item**2 # Powering the item to 2
+        print(f'{item:.1f}')
